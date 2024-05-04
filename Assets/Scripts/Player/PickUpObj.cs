4732d6b6e9df5092e8c61185b243b63bb8fc1903
José Miguel Aranda Fernández <josemi.arandafernandez@gmail.com>
2024-05-04 17:31:26 +0200

    added picking up objects

---
+++ Assets/Scripts/Player/PickUpObj.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpObj : MonoBehaviour
+{
+    [SerializeField] public Transform playerCameraTransform;
+    [SerializeField] public Transform objectGrabPointTransform;
+    [SerializeField] public LayerMask pickUpLayerMask;
+
+    private ObjectGrabbable objectGrabbable;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E)) {
+            if(objectGrabbable == null) { 
+                // Not carring a object, try to grab
+                float pickUpDistance = 2f;
+               if (Physics.Raycast(playerCameraTransform.position, playerCameraTransform.forward, out RaycastHit raycastHit, pickUpDistance, pickUpLayerMask))
+                {
+                    if(raycastHit.transform.TryGetComponent(out objectGrabbable))
+                    {
+                        objectGrabbable.Grab(objectGrabPointTransform);
+                    }
+                }
+            }
+            else
+            {
+                // Currently carrying something, drop
+                objectGrabbable.Drop();
+                objectGrabbable = null;
+            }
+        }
+    }
+}
