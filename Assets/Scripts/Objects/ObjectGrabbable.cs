546f45e4534177258ef811572961d8748dc18068
José Miguel Aranda Fernández <josemi.arandafernandez@gmail.com>
2024-05-18 11:48:05 +0200

    added scroll distance

--- Assets/Scripts/Objects/ObjectGrabbable.cs
+++ Assets/Scripts/Objects/ObjectGrabbable.cs
@@ -7,9 +7,15 @@ public class ObjectGrabbable : MonoBehaviour
     private Rigidbody objectRigidBody;
     private Transform objectGrabPointTransform;
     public string value = "Test";
+    private float scrollSensitivity = 150f;
+    private float lerpSpeed = 10f;
+    private float minDistance = 1f;
+    private float maxDistance = 3f;
+    private float currentDistance = 2f; 
+
     private void Awake()
     {
-        objectRigidBody = GetComponent<Rigidbody>();        
+        objectRigidBody = GetComponent<Rigidbody>();
     }
 
     public void Grab(Transform objectGrabPointTransform)
@@ -26,12 +32,19 @@ public class ObjectGrabbable : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if(objectGrabPointTransform != null)
+        if (objectGrabPointTransform != null)
         {
-            float lerpSpeed = 10f;
-            Vector3 newPosition = Vector3.Lerp(transform.position, objectGrabPointTransform.position, Time.deltaTime * lerpSpeed);
+            float scrollWheelValue = Input.GetAxis("Mouse ScrollWheel"); // Obtain 0.1 or -0.1 depending on scroll mouse wheel
+            
+            currentDistance += scrollWheelValue * scrollSensitivity * Time.deltaTime;
+
+            currentDistance = Mathf.Clamp(currentDistance, minDistance, maxDistance); 
+
+            Camera mainCamera = Camera.main;
+            Vector3 targetPosition = mainCamera.transform.position + mainCamera.transform.forward * currentDistance;
+            Vector3 newPosition = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * lerpSpeed);
+
             objectRigidBody.MovePosition(newPosition);
         }
     }
-
 }
