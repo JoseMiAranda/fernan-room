95908164e4c6cd66ff04d28604cbce420c188c8c
José Miguel Aranda Fernández <josemi.arandafernandez@gmail.com>
2024-05-18 19:59:58 +0200

    added round & guidance text

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,14 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using Unity.VisualScripting;
 
 public class GameManager : MonoBehaviour
 {
     private string currentMessage = "Text Message";
     public static GameManager Instance;
+    private static TextMeshProUGUI roundText;
+    private static TextMeshProUGUI guidanceText;
+
+    private const string SHOW_BOARD = "Pulse E sobre la pizarra";
+
     private void Awake()
     {
-        if(Instance == null)
+        roundText = GameObject.FindWithTag("Round").GetComponent<TextMeshProUGUI>();
+        guidanceText = GameObject.FindWithTag("Guidance").GetComponent<TextMeshProUGUI>();
+
+        if (Instance == null)
         {
             Instance = this;
             RoundOne();
@@ -21,13 +30,17 @@ public class GameManager : MonoBehaviour
 
     public void RoundOne()
     {
-        Debug.Log("ROUND ONE!!!");
+        roundText.text = "Ronda 1";
+        guidanceText.text = SHOW_BOARD;
+
         currentMessage = "Find a cube!!!!";
     }
 
     public void RoundTwo()
     {
-        Debug.Log("ROUND TWO!!!");
+        roundText.text = "Ronda 2";
+        guidanceText.text = SHOW_BOARD;
+
         currentMessage = "Take the weapon!!!";
     }
 
