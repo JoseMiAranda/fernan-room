9746a7c1474946a5e9eb7ef150377a862c663b0f
José Miguel Aranda Fernández <josemi.arandafernandez@gmail.com>
2024-05-18 18:50:15 +0200

    added decoration -> part 4

--- Assets/Scripts/Scanner/Scanner.cs
+++ Assets/Scripts/Scanner/Scanner.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 
 public class Scanner : MonoBehaviour
 {
-    private string findedObj = "Cube";
+    private string findedObj = "Tezla";
     private bool readingObject = false;
     private bool stopReading = false;
 
@@ -15,20 +15,20 @@ public class Scanner : MonoBehaviour
     {
         if(!stopReading && !readingObject)
         {
-            ObjectGrabbable objectGrabbable = collision.gameObject.GetComponent<ObjectGrabbable>();
-        
+            ObjectGrabbable objectGrabbable = collision.gameObject.GetComponent<ObjectGrabbable>(); // Scanner only reads ObjectGrabbable objects
+
             if (objectGrabbable != null)
             {
                 readingObject = true;
                 if (objectGrabbable.value == findedObj)
                 {
-                    Debug.Log("That's a Cube");
+                    Debug.Log("That's a coffee maker");
                     stopReading = true;
                     GameManager.Instance.RoundTwo();
                 }
                 else
                 {
-                    Debug.Log("That's not a Cube");
+                    Debug.Log("That's not a coffe maker");
                 }
                 Timer timer = new(ResetRead, null, 1000, 0);
             }
